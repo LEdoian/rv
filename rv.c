01aa93675994a66914700792a866a2a29359f635
LEdoian <tap9701@gmail.com>
2013-11-29 20:55:33 +0100

    That change again, I forgot to compile and it threw errors, so i fixed it ;)

--- rv.c
+++ rv.c
@@ -3,12 +3,12 @@
 
 int main (int argc, char *argv[]){
 
-char string[];
+char string[1000]; //This needs to be changed as soon as I have my book back :)
 int i;
 
 for( i=1; i < argc; i++){
 	sprintf(string, "%s %s", string, argv[i]);
 	}
-fprintf(stderr, "\nReturn value: %d\n", system(string);
+fprintf(stderr, "\nReturn value: %d\n", system(string));
 return EXIT_SUCCESS;
 }
