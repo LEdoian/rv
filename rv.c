3b17a438c7679b918f5ade885523a71c9e5a8d1a
LEdoian <01@example.com>
2014-07-28 21:29:14 +0200

    lal

--- rv.c
+++ rv.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main (int argc, char *argv[]){
 
-char string[1000] = {"\0"}; //This needs to be changed as soon as I have my book back :)
-int i;
+int i;					/*For for cycles*/
+int q = 0;				/*for swapping between the two "strings"*/
+int sizeofstring = 0;
+
+for(i = 1; i < argc; i++){
+	  sizeofstring += strlen(argv[i]) + 1;
+	  }
+char *string = malloc(sizeofstring);
+char *string2 = malloc(sizeofstring);
 
 for( i=1; i < argc; i++){
-	sprintf(string, "%s %s", string, argv[i]);
-	}
-//printf("%s\n", string);    //That was just debug, it should be deleted
+	 if(q == 0){
+	 	sprintf(string2, "%s %s", string, argv[i]);
+	 	q = 1;
+		}
+     if(q == 1){
+	 	  sprintf(string, "%s %s", string2, argv[i]);
+		  q = 0; 
+	   	  }
+	 }
 fprintf(stderr, "\nReturn value: %d\n", system(string));
 return EXIT_SUCCESS;
 }
