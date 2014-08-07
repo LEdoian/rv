5f8cf709cc6e82c94b5fdeef82bbdfc9a682eef0
LEdoian <tap9701@gmail.com>
2014-08-07 13:16:06 +0200

    Now fully functional, v. 1.0

--- rv.c
+++ rv.c
@@ -9,21 +9,14 @@ int q = 0;				/*for swapping between the two "strings"*/
 int sizeofstring = 0;
 
 for(i = 1; i < argc; i++){
-	  sizeofstring += strlen(argv[i]) + 1;
-	  }
+	sizeofstring += strlen(argv[i]) + 1;
+	}
 char *string = malloc(sizeofstring);
-char *string2 = malloc(sizeofstring);
 
-for( i=1; i < argc; i++){
-	 if(q == 0){
-	 	sprintf(string2, "%s %s", string, argv[i]);
-	 	q = 1;
-		}
-     if(q == 1){
-	 	  sprintf(string, "%s %s", string2, argv[i]);
-		  q = 0; 
-	   	  }
-	 }
+for( i=1; i < argc; i++){		/*We don't want to include the name of the command*/
+	strcat(string, argv[i]);
+	strcat(string, " ");
+	}
 fprintf(stderr, "\nReturn value: %d\n", system(string));
 return EXIT_SUCCESS;
 }
